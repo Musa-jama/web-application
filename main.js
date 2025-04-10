d41bf6b57e3a888fc7ce84d849fd47a415d92f0b
musajama <musanuh36@gmail.com>
2025-04-10 17:14:31 -0400

    weather data on the weather page

---
+++ main.js
@@ -0,0 +1,23 @@
+import { navigate } from './modules/router.js';
+import { displayWeather } from './modules/weather.js';
+
+document.getElementById('home-btn').addEventListener('click', () => {
+  navigate('home');
+});
+
+document.getElementById('about-btn').addEventListener('click', () => {
+  navigate('about');
+});
+
+document.getElementById('contact-btn').addEventListener('click', () => {
+  navigate('contact');
+});
+
+
+document.getElementById('weather-btn').addEventListener('click', () => {
+  navigate('weather');
+  displayWeather('London'); 
+});
+
+
+navigate('home');
